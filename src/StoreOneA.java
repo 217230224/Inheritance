2c431a89f4206eb380a3900bc87ae9ee9011a10a
217230224 <x00217230224@njit.edu.cn>
2025-03-22 13:27:22 +0800

    ex.10

---
+++ src/StoreOneA.java
@@ -0,0 +1,35 @@
+public class StoreOneA extends StoreToRent {
+
+    private boolean specialCustomer;
+    private double monthlyPayment;
+    private static final double DISCOUNT_RATE = 0.1;
+
+    public StoreOneA(boolean loanRequired, double loanAmount, int loanPaymentTerm, boolean specialCustomer) {
+        super(loanRequired, loanAmount, loanPaymentTerm);
+        this.specialCustomer = specialCustomer;
+    }
+
+    public void showStoreInfo() {
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "**LOAN DETAILS (if applicable):**\n" +
+                "Loan Amount: " + getLoanAmount() + "\n" +
+                "Loan Payment Term: " + getLoanPaymentTerm() + "\n" +
+                "Interest Rate: " + getINTEREST_RATE() + "\n" +
+                "Special Customer: " + specialCustomer + "\n" +
+                "Monthly Loan Payment: " + monthlyPayment + "\n";
+    }
+
+    @Override
+    public double calculateLoanFinancing() {
+        monthlyPayment = super.calculateLoanFinancing();
+        if (specialCustomer) {
+            monthlyPayment -= (monthlyPayment * DISCOUNT_RATE);
+        }
+        return monthlyPayment;
+    }
+}
